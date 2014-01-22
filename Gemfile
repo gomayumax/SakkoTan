6970b65aaca9c3d98ae314ef5669f928c87cb98d
hoto <hoto17296@gmail.com>
2014-01-22 11:29:45 +0900

    sakkotan

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 gem 'sinatra'
 gem 'sinatra-jsonp'
+gem 'yahoo_parse_api'
--- app.rb
+++ app.rb
@@ -1,13 +1,45 @@
 require 'bundler/setup'
 require 'sinatra/base'
 require 'sinatra/jsonp'
+require 'yahoo_parse_api'
+
+YahooParseApi::Config.app_id = 'dj0zaiZpPVhOT2VrU00xRVZ2dyZzPWNvbnN1bWVyc2VjcmV0Jng9NmQ-'
 
 class SakkoTan < Sinatra::Base
   helpers Sinatra::Jsonp
 
   get '/' do
     str = params.key?('str') ? params['str'] : ''
-    data = { str: str }
-    jsonp data
+    reading = reading(str)
+    sakkotan = sakkotan(reading)
+    result = { str: str, reading: reading, sakkotan: sakkotan }
+    jsonp result
+  end
+
+  def reading(str)
+    parsed = YahooParseApi::Parse.new.parse(str)
+    parsed = parsed['ResultSet']['ma_result']['word_list']['word']
+    
+    reading = ''
+    if parsed.instance_of?(Array)
+      parsed.map {|word| reading += word['reading'] }
+    elsif parsed.instance_of?(Hash)
+      reading += parsed['reading']
+    else
+      abort '形態素解析失敗(´・ω・｀)'
+    end
+
+    reading
+  end
+
+  def sakkotan(str)
+    pattern = {
+      'さ' => 'た',
+      'し' => 'てぃ',
+      'す' => 'てゅ',
+      'せ' => 'て',
+      'そ' => 'と'
+    }
+    str.gsub(/[さしすせそ]/, pattern)
   end
 end
